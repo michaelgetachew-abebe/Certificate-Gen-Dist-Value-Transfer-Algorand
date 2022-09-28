b9b4b3b7d0ac38e064eba8c862b0bb8f81792270
michaelgetachew <mikygetyos@gmail.com>
2022-09-29 01:31:59 +0300

    Feat(add): started and added coustomizing codes on the boiler plate template codes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={10} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          10 Academy Certification Platform
-        </a>
-      </header>
+      
     </div>
   );
 }
