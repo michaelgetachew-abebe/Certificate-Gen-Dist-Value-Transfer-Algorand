c3d97d01e709e7e8d70a750c1eb705511035229d
michaelgetachew <mikygetyos@gmail.com>
2022-09-29 13:03:54 +0300

    Feat(add): added the some starter code for the index page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+      <>
+      <h1>10 ACADEMY SMART CERTIFICATION PLATFORM</h1>
+      <h3>Secure and Smart Certificate Generation, Distribution and Value Transfer</h3>
+      <button onClick = {() => alert('Clicked, Heading to the Admin Panel....')}>Admin</button>
+      <button onClick = {() => alert('Clicked, Heading to the Trainee Panel..')}>Trainee</button>
+      </>
     </div>
   );
 }
