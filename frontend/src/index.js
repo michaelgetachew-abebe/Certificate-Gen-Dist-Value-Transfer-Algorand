2f92b64209820a5fbd1412330b113f0e9fcf59e3
michaelgetachew <mikygetyos@gmail.com>
2022-09-29 13:06:47 +0300

    Feat(add): started customizing the front page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// styles
+import 'bootstrap/scss/bootstrap.scss';
+import 'assets/scss/paper-kit.scss?v=1.3.0';
+import 'assets/demo/demo.css?v=1.3.0';
+// pages
+import LandingPage from 'views/LandingPage.js';
+import AdminView from 'views/AdminView';
+import Typography from 'views/Typography.js';
+import TraineeView from 'views/TraineeView';
+
+// others
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/admin" element={<AdminView />} />
+      <Route path="/trainee" element={<TraineeView />} />
+      <Route path="/typo" element={<Typography />} />
+
+      {/* <Route
+        path="/register-page"
+        render={(props) => <RegisterPage {...props} />}
+      /> */}
+    </Routes>
+    {/* <Navigate to="/" /> */}
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
